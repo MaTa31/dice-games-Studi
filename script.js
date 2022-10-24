61e3b143b95f0e78c512cd3cec1ddf02d3917129
MaTa31 <mickael-fernandez@live.fr>
2022-10-24 15:43:51 +0200

    V1.2 Version Beta

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 $(document).ready(function () {
 
-    
-    var CurrentScr = 0
-    var FinalScr = 0    
-    var activePlayer = 1
 
-    $(".box.has-text-centered").css({'background-image': 'linear-gradient(-90deg, rgba(255,255,255,1) 30%, rgba(177,177,177,1) 100%)'}); 
-    $("#Player1").addClass("active");     
 
+    function init() {
+
+        CurrentScr = 0
+        FinalScr = 0
+        activePlayer = 1
+        $("#CurrentP1, #CurrentP2, #ScoreP1, #ScoreP2").text(0)
+        $(".box.has-text-centered").css({ 'background-image': 'linear-gradient(-90deg, rgba(255,255,255,1) 30%, rgba(177,177,177,1) 100%)' });
+        $("#Player1").addClass("active");
+
+    }
+    function nextPlayer() {
+
+        if (activePlayer === 1) {
+
+            $(".box.has-text-centered").css({ 'background-image': 'linear-gradient(90deg, rgba(255,255,255,1) 30%, rgba(177,177,177,1) 100%)' });
+            $("#Player1").removeClass("active");
+            $("#Player2").addClass("active");
+            activePlayer = 2
+
+        }
+        else {
+
+            $(".box.has-text-centered").css({ 'background-image': 'linear-gradient(-90deg, rgba(255,255,255,1) 30%, rgba(177,177,177,1) 100%)' });
+            $("#Player2").removeClass("active");
+            $("#Player1").addClass("active");
+            activePlayer = 1
+        }
+
+
+    }
+
+    init()
 
 
     $("#newgame").on("click", function () {
 
-        CurrentScr = 0
-        FinalScr = 0    
-        activePlayer = 1
-        $("#CurrentP1, #CurrentP2, #ScoreP1, #ScoreP2").text(0)
+        init()
+        $("#hold").show()
+        $("#roll").show()
 
     });
 
@@ -24,55 +49,47 @@ $(document).ready(function () {
     $("#roll").on("click", function () {
 
         var resultdice = Math.floor((Math.random() * 6) + 1);
-        
-
         $(".dice").attr("class", "dice");
+
         setTimeout(function () {
             $(".dice").addClass("roll-" + resultdice);
         }, 1);
 
-        if (resultdice === 1) { 
+        if (resultdice === 1) {
 
-            $("#CurrentP"+activePlayer).text(0)
+            $("#CurrentP" + activePlayer).text(0);
+            CurrentScr = 0;
+            FinalScr = 0;
 
-            if (activePlayer === 1) {
-
-                $(".box.has-text-centered").css({'background-image': 'linear-gradient(90deg, rgba(255,255,255,1) 30%, rgba(177,177,177,1) 100%)'});
-                $("#Player1").removeClass("active"); 
-                $("#Player2").addClass("active"); 
-                activePlayer = 2
-
-            }
-            else {
-
-                $(".box.has-text-centered").css({'background-image': 'linear-gradient(-90deg, rgba(255,255,255,1) 30%, rgba(177,177,177,1) 100%)'});
-                $("#Player2").removeClass("active"); 
-                $("#Player1").addClass("active");
-                activePlayer = 1
-            }
-
+            nextPlayer();
         }
 
-        else { 
+        else {
 
-            CurrentScr += resultdice
-            $("#CurrentP"+activePlayer).text(CurrentScr)
+            CurrentScr += resultdice;
+            $("#CurrentP" + activePlayer).text(CurrentScr);
+            $("#hold").on("click", function (event) {
 
-            $("#hold").on("click", function () {
-
-                FinalScr += CurrentScr
-                $("#ScoreP"+activePlayer).text(FinalScr)
-                CurrentScr = 0
-                $("#CurrentP"+activePlayer).text(0)
+                event.stopImmediatePropagation();
+                FinalScr = parseInt($("#ScoreP" + activePlayer).html()) + CurrentScr;
+                $("#ScoreP" + activePlayer).text(FinalScr);
+                CurrentScr = 0;
+                $("#CurrentP" + activePlayer).text(0);
 
                 if (FinalScr >= 100) {
 
-                // Modal
+                    $("#ScoreP" + activePlayer).text("WINNER !!");
+                    $("#hold").hide();
+                    $("#roll").hide();
+
+
 
                 }
-       
+
+                nextPlayer()
+
             })
-        }    
+        }
     })
 });
 
